867329a6a1e4f5ca645b332d353e9c23fca971bb
Shuhei Nitta <huisintheta@gmail.com>
2022-09-25 12:08:00 +0900

    ✨ Make `get_date_annotation` take `date` of tuple (#23)

--- tests/test_figure.py
+++ tests/test_figure.py
@@ -8,12 +8,25 @@ from tlab_pptx import figure
 
 @pytest.mark.parametrize(
     "date",
-    [datetime.date(2022, 1, 1), datetime.date(2023, 12, 31)],
+    [datetime.date(2022, 1, 1), datetime.date(2023, 12, 31), (2022, 1, 1)],
 )
-def test_get_date_annotaion(date: datetime.date) -> None:
+def test_get_date_annotaion(date: datetime.date | tuple[int, int, int]) -> None:
     annotation = figure.get_date_annotation(date)
+    if isinstance(date, tuple):
+        date = datetime.date(*date)
     assert annotation["text"] == date.strftime("%Y.%m.%d")
     assert go.layout.Annotation(annotation)  # TODO: Expect assert not raises ValueError
+
+
+@pytest.mark.parametrize(
+    "date",
+    [(2022, 13, 1), (2022, 1)],
+)
+def test_get_date_annotaion_with_incompiatible_tuple(
+    date: datetime.date | tuple[int, int, int]
+) -> None:
+    with pytest.raises(ValueError):
+        figure.get_date_annotation(date)
 
 
 def test_get_default_layout() -> None:
