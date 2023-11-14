abfd191543ff7539b012b45255e39dcc99b15661
Shuhei Nitta <huisintheta@gmail.com>
2023-11-15 02:47:20 +0900

    🧑‍💻 Improve developer experience (#114)

---
+++ .devcontainer/create-hatch-env.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+set -euC
+
+hatch env create
+SETTING_JSON_PATH="$HOME/.vscode-server/data/Machine/settings.json"
+jq '. + { "python.defaultInterpreterPath": "'"$(hatch env find)"'/bin/python"}' "$SETTING_JSON_PATH" \
+    | tee "$SETTING_JSON_PATH.tmp"
+mv "$SETTING_JSON_PATH.tmp" "$SETTING_JSON_PATH"
--- scripts/test.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-set -euC
-set -o pipefail
-set -x
-
-pytest --doctest-modules src  # Uncomment when to run doctests
-pytest --cov-report=term-missing:skip-covered "${@}"
