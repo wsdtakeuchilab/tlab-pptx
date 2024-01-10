57a3b82723b0b4f01e5714d19498d38eb4a15d42
Shuhei Nitta <huisintheta@gmail.com>
2024-01-10 21:11:03 +0000

    Update create-hatch-env.sh

--- .devcontainer/create-hatch-env.sh
+++ .devcontainer/create-hatch-env.sh
@@ -4,6 +4,6 @@ set -euC
 
 hatch env create
 SETTING_JSON_PATH="$HOME/.vscode-server/data/Machine/settings.json"
-jq '. + { "python.defaultInterpreterPath": "'"$(hatch env find)"'/bin/python"}' "$SETTING_JSON_PATH" \
-    | tee "$SETTING_JSON_PATH.tmp"
+jq '. + { "python.defaultInterpreterPath": "'"$(hatch env find)"'/bin/python"}' "$SETTING_JSON_PATH" |
+  tee "$SETTING_JSON_PATH.tmp"
 mv "$SETTING_JSON_PATH.tmp" "$SETTING_JSON_PATH"
